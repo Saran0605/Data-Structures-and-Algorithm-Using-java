788487637370f77a769fa42ae088ceceb4286b7c
Saran S <141417701+Saran0605@users.noreply.github.com>
2025-01-08 22:59:48 +0530

    Created prg1.java
    DATE - 08-01-2025

---
+++ prg1.java
@@ -0,0 +1,27 @@
+//Finding missing no in an array 
+import java.util.*;
+public class prg1 {
+    public static int missing(int[] arr, int n){
+        //Formula for finding sum of series
+        int sum = n*(n+1)/2;
+        //Finding total value of array
+        int total = 0;
+        for(int num:arr){
+            total+=num;
+        }
+        //find value by subtracting sum of series value and total sum of array
+        return sum-total;
+    }
+    public static void main(String[] args){
+        //use scanner for input purposes
+        int[] arr = {1,2,3,5,6};
+        int n = 6;
+        int res = missing(arr,n);
+        System.out.println(res);
+    }
+    
+    
+}
+
+//OUTPUT : 4;
+//TIME COMPLEXITY : O(n) -> one for loop for adding elements
