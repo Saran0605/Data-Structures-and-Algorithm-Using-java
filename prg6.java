dd65900e15b97b2fe429a5bc1755ec2c62125cfa
Saran S <141417701+Saran0605@users.noreply.github.com>
2025-01-12 16:50:25 +0530

    Created prg6.java
    Date - 12-01-2025

---
+++ prg6.java
@@ -0,0 +1,36 @@
+//Merging two sorted arrays into one
+import java.util.*;
+public class prg6 {
+    public static void main(String[] args){
+        int arr1[] = {1,3,5,7};
+        int arr2[] = {2,4,6,8,9,20};
+        merge(arr1,arr2);
+    }
+    public static void merge(int[] arr1,int[] arr2){
+        int i = 0;
+        int j = 0;
+        List<Integer> li = new ArrayList<>();
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<=arr2[j]){
+                li.add(arr1[i++]);
+
+            }
+            else{
+                li.add(arr2[j++]);
+            }
+        }
+
+        while(i<arr1.length){
+            li.add(arr1[i++]);
+        }
+        while(j<arr2.length){
+            li.add(arr2[j++]);
+        }
+        System.out.println(li);
+
+    }
+    
+}
+
+
+//time and space = O(n+m) = > size of arr1 and arr2
