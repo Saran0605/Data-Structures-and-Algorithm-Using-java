23f4d3bca418598e33385cd2a7bd6c123ee5e4eb
Saran S <141417701+Saran0605@users.noreply.github.com>
2025-02-03 22:43:38 +0530

    Create prg10.java
    Date - 03/02/2025

---
+++ prg10.java
@@ -0,0 +1,32 @@
+//Count and say problem
+public class prg10 {
+    public static void main(String[] args){
+        int n = 5;
+        String s = count(n);
+        System.out.println(s);
+    }
+    public static String count(int n){
+        if(n==1){
+            return "1";
+        }
+        String pre = count(n-1);
+        return getseq(pre);
+
+    }
+    public static String getseq(String pre){
+        StringBuilder s = new StringBuilder();
+        int i = 0;
+        int len = pre.length();
+        while(i<len){
+            int count = 1;
+            while(i+1 <len && pre.charAt(i)==pre.charAt(i+1)){
+                count++;
+                i++;
+            }
+            s.append(count).append(pre.charAt(i));
+            i++;
+        }
+        return s.toString();
+    }
+    
+}
